deeb999936309698f74a69820dba9cab6e32ce0f
yshalsager <ysh-alsager@hotmail.com>
2024-07-13 21:37:50 +0300

    feat(modules): Add message JSON dump
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

---
+++ src/modules/debug.py
@@ -0,0 +1,30 @@
+import orjson
+from telethon.events import NewMessage
+
+from src.modules.base import ModuleBase
+
+json_options = (
+    orjson.OPT_INDENT_2 | orjson.OPT_SORT_KEYS | orjson.OPT_NAIVE_UTC | orjson.OPT_OMIT_MICROSECONDS
+)
+
+
+class Debug(ModuleBase):
+    @staticmethod
+    async def to_json(event: NewMessage.Event) -> None:
+        await event.reply(
+            f'<code>{orjson.dumps(event.message.to_dict(), option=json_options).decode()}</code>',
+        )
+
+    @property
+    def name(self) -> str:
+        return 'Debug'
+
+    @property
+    def description(self) -> str:
+        return 'Print message to JSON.'
+
+    def commands(self) -> ModuleBase.CommandsT:
+        return {'json': {'handler': self.to_json, 'description': self.description}}
+
+    def is_applicable(self, event: NewMessage.Event) -> bool:
+        return bool(event.message.text.startswith('/json'))
