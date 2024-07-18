b40a3577247139695e287d9a67189994cc4c8684
yshalsager <ysh-alsager@hotmail.com>
2024-07-18 21:52:26 +0300

    fix(utils): Correct return typehint of get_reply_message
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- src/utils/telegram.py
+++ src/utils/telegram.py
@@ -1,9 +1,10 @@
 from telethon.events import CallbackQuery, NewMessage
+from telethon.tl.custom import Message
 
 
 async def get_reply_message(
     event: NewMessage.Event | CallbackQuery.Event, previous: bool = False
-) -> NewMessage.Event:
+) -> Message:
     if isinstance(event, CallbackQuery.Event):
         message = await event.get_message()
         return message if not previous else await message.get_reply_message()
