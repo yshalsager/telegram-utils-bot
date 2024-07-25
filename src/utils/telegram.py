13f2955d9b855e68c70301d12f7d195945e3cbe4
yshalsager <ysh-alsager@hotmail.com>
2024-07-25 19:12:46 +0300

    fix(utils/telegram): upload and send file instead of using payload
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- src/utils/telegram.py
+++ src/utils/telegram.py
@@ -1,9 +1,12 @@
 from asyncio import sleep
-from io import BytesIO
+from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 from telethon.errors import MessageTooLongError
 from telethon.events import CallbackQuery, NewMessage
 from telethon.tl.custom import Message
+
+from src.utils.downloads import upload_file
 
 
 async def get_reply_message(
@@ -16,14 +19,23 @@ async def get_reply_message(
 
 
 async def edit_or_send_as_file(
-    event: NewMessage.Event, message: Message, text: str, file_name: str = 'output.txt'
+    event: NewMessage.Event,
+    message: Message,
+    text: str,
+    file_name: str = 'output.txt',
+    caption: str = '',
 ) -> None:
     try:
         await message.edit(text)
     except MessageTooLongError:
-        file = BytesIO(text.encode())
-        file.name = file_name
-        await event.client.send_file(message.chat_id, file, reply_to=message.id)
+        progress_message = await event.reply('Sending file...')
+        with NamedTemporaryFile(mode='w+', suffix='.txt') as temp_file:
+            temp_file.write(text)
+            temp_file.flush()
+            temp_file_path = Path(temp_file.name)
+            temp_file_path = temp_file_path.rename(temp_file_path.with_name(file_name))
+            await upload_file(event, temp_file_path, progress_message, caption=caption)
+            await progress_message.delete()
 
 
 async def delete_message_after(message: Message, seconds: int = 10) -> None:
