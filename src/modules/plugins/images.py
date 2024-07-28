9e205e0bdf9336b0a8fb0f7d30df96464902eb00
yshalsager <ysh-alsager@hotmail.com>
2024-07-28 14:16:18 +0300

    feat(modules/images): Add tesseract ocr
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- src/modules/plugins/images.py
+++ src/modules/plugins/images.py
@@ -15,7 +15,8 @@ from src.utils.command import Command
 from src.utils.downloads import download_file, upload_file
 from src.utils.filters import has_photo_or_photo_file
 from src.utils.images import crop_image_white_borders
-from src.utils.telegram import delete_message_after, get_reply_message
+from src.utils.run import run_command
+from src.utils.telegram import delete_message_after, edit_or_send_as_file, get_reply_message
 
 ALLOWED_INPUT_FORMATS = {
     'jpg',
@@ -96,8 +97,32 @@ async def trim_image(event: NewMessage.Event) -> None:
         output_file.unlink(missing_ok=True)
 
 
+async def ocr_image(event: NewMessage.Event) -> None:
+    reply_message = await get_reply_message(event, previous=True)
+    status_message = await event.reply('Starting process...')
+    progress_message = await event.reply('Performing OCR on image...')
+    lang = 'ara'
+    if matches := Images.commands['image ocr'].pattern.search(reply_message.raw_text):
+        lang = matches[-1] if len(matches.groups()) > 2 else lang
+
+    with NamedTemporaryFile(dir=TMP_DIR, suffix=reply_message.file.ext) as temp_file:
+        temp_file_path = await download_file(event, temp_file, reply_message, progress_message)
+        command = f'tesseract "{temp_file_path}" "{temp_file_path.stem}" -l {lang}'
+        output_file = temp_file_path.with_suffix('.txt')
+        _, code = await run_command(command, cwd=temp_file_path.parent)
+        if code == 0 and output_file.exists() and output_file.stat().st_size:
+            await edit_or_send_as_file(event, status_message, output_file.read_text())
+            output_file.unlink(missing_ok=True)
+        else:
+            await status_message.edit('Failed to OCR.')
+            return
+
+    await progress_message.edit('Image OCR process complete.')
+
+
 handlers: CommandHandlerDict = {
     'image convert': convert_image,
+    'image ocr': ocr_image,
     'image trim': trim_image,
 }
 
@@ -113,6 +138,14 @@ class Images(ModuleBase):
             handler=handler,
             description='[format] - Convert image to another format',
             pattern=re.compile(r'^/(image)\s+(convert)\s+([\d\w]{3,4})$'),
+            condition=has_photo_or_photo_file,
+            is_applicable_for_reply=True,
+        ),
+        'image ocr': Command(
+            name='image ocr',
+            handler=handler,
+            description='[lang]: Perform OCR using tesseract',
+            pattern=re.compile(r'^/(image)\s+(ocr)\s+?([\w+]{3,})?$'),
             condition=has_photo_or_photo_file,
             is_applicable_for_reply=True,
         ),
