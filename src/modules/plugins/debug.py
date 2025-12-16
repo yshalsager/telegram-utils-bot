f09c629fa779594eef7dba599c44cd30d7c0453f
yshalsager <ysh-alsager@hotmail.com>
2025-12-16 15:43:39 +0200

    feat(plugins/debug): enhance JSON reply handling with file upload support

--- src/modules/plugins/debug.py
+++ src/modules/plugins/debug.py
@@ -1,3 +1,6 @@
+from html import escape as html_escape
+from pathlib import Path
+from tempfile import NamedTemporaryFile
 from typing import ClassVar
 
 import orjson
@@ -6,6 +9,7 @@ from telethon.events import NewMessage
 
 from src.modules.base import ModuleBase
 from src.utils.command import Command
+from src.utils.downloads import upload_file
 from src.utils.filters import is_reply_in_private
 from src.utils.i18n import t
 from src.utils.json_processing import json_options, process_dict
@@ -14,7 +18,19 @@ from src.utils.json_processing import json_options, process_dict
 async def to_json(event: NewMessage.Event) -> None:
     reply_message = await event.get_reply_message()
     json_str = orjson.dumps(process_dict(reply_message.to_dict()), option=json_options).decode()
-    await event.reply(f'<pre>{json_str}</pre>')
+    json_html = html_escape(json_str)
+    if len(json_html) > 3500:
+        progress_message = await event.reply(t('sending_file'))
+        with NamedTemporaryFile(mode='w+', suffix='.json') as temp_file:
+            temp_file.write(json_str)
+            temp_file.flush()
+            path = Path(temp_file.name)
+            path = path.rename(path.with_name('message.json'))
+            await upload_file(event, path, progress_message, force_document=True)
+        await progress_message.delete()
+        return
+
+    await event.reply(f'<pre>{json_html}</pre>')
 
 
 class Debug(ModuleBase):
